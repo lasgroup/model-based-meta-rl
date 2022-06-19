520eb33dc8b04d23f008170ff75a1820f714c8b5
Arjun Bhardwaj <arbhardwaj98@gmail.com>
2022-06-19 20:21:42 +0200

    Updated default network paramters

--- experiments/lib_environments/parser.py
+++ experiments/lib_environments/parser.py
@@ -59,10 +59,10 @@ def get_argument_parser() -> argparse.ArgumentParser:
     # Model parameters
     parser.add_argument("--model-kind", type=str, default="ProbabilisticEnsemble")
     parser.add_argument("--model-num-heads", type=int, default=5)
-    parser.add_argument("--model-layers", type=int, nargs="*", default=[64, 64])
-    parser.add_argument("--model-non-linearity", type=str, default="ReLU")
-    parser.add_argument("--model-unbiased-head", action="store_false")  # TODO: Should be store_true
-    parser.add_argument("--model-opt-lr", type=float, default=1e-3)
+    parser.add_argument("--model-layers", type=int, nargs="*", default=[256, 256])
+    parser.add_argument("--model-unbiased-head", action="store_true")
+    parser.add_argument("--model-non-linearity", type=str, default="Tanh")
+    parser.add_argument("--model-opt-lr", type=float, default=3e-4)
     parser.add_argument("--model-opt-weight-decay", type=float, default=0)
     parser.add_argument("--model-learn-num-iter", type=int, default=50)
     parser.add_argument("--model-learn-batch-size", type=int, default=32)
@@ -77,15 +77,15 @@ def get_argument_parser() -> argparse.ArgumentParser:
     parser.add_argument("--sim-memory-num-trajectories", type=int, default=0)
 
     # Value function parameters
-    parser.add_argument("--value-function-layers", type=int, nargs="*", default=[64, 64])
-    parser.add_argument("--value-function-unbiased-head", action="store_false")  # TODO: Should be store_true
-    parser.add_argument("--value-function-non-linearity", type=str, default="ReLU")
+    parser.add_argument("--value-function-layers", type=int, nargs="*", default=[256, 256])
+    parser.add_argument("--value-function-unbiased-head", action="store_true")
+    parser.add_argument("--value-function-non-linearity", type=str, default="Tanh")
     parser.add_argument("--value-function-tau", type=float, default=0)
 
     # Value function parameters
-    parser.add_argument("--policy-layers", type=int, nargs="*", default=[64, 64])
-    parser.add_argument("--policy-unbiased-head", action="store_false")  # TODO: Should be store_true
-    parser.add_argument("--policy-non-linearity", type=str, default="ReLU")
+    parser.add_argument("--policy-layers", type=int, nargs="*", default=[256, 256])
+    parser.add_argument("--policy-unbiased-head", action="store_true")
+    parser.add_argument("--policy-non-linearity", type=str, default="Tanh")
     parser.add_argument("--policy-tau", type=float, default=0)
     parser.add_argument("--policy-deterministic", action="store_true")
 
