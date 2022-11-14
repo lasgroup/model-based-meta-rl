dd04647ac39b4546100b087a67caeda011d9b2c2
Arjun Bhardwaj <arbhardwaj98@gmail.com>
2022-11-14 08:46:19 +0100

    3D reacher environment

--- lib/environments/random_mujocoMB_envs/__init__.py
+++ lib/environments/random_mujocoMB_envs/__init__.py
@@ -4,4 +4,5 @@ from .envs.random_pusher import RandomMBPusherEnv
 from .envs.random_reacher import RandomMBReacherEnv
 from .envs.random_pendulum import RandomMBPendulumEnv
 from .envs.random_cart_pole import RandomMBCartPoleEnv
+from .envs.random_reacher3d import RandomMBReacher3DEnv
 from .envs.random_half_cheetah import RandomMBHalfCheetahEnv
