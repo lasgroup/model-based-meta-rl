4183af3731207a80fe774089887be1a5fe490387
Arjun Bhardwaj <arbhardwaj98@gmail.com>
2023-05-15 14:33:49 +0200

    Parallel PACOH Regression for RCCar

--- experiments/rccar_experiments/pacoh_regression.py
+++ experiments/rccar_experiments/pacoh_regression.py
@@ -51,6 +51,21 @@ def process_data(states_list, actions_list, next_states_list):
     return states_actions, next_states
 
 
+def eval_pacoh(evaluator):
+    pacoh_eval_mean_per_task_ids = [eval_pacoh_per_task.remote(evaluator, task_num) for task_num in range(evaluator.n_tasks)]
+    pacoh_eval_mean_per_task = ray.get(pacoh_eval_mean_per_task_ids)
+    metrics_mean, metrics_std = PACOH_NN_Regression._aggregate_eval_metrics_across_tasks(pacoh_eval_mean_per_task)
+    return metrics_mean, metrics_std
+
+
+@ray.remote
+def eval_pacoh_per_task(evaluator, task_num):
+    context_data, test_data = evaluator.setup_test_data(task_num)
+    pacoh_model = evaluator.setup_pacoh_model(task_num)
+    pacoh_task_eval, _ = pacoh_model.meta_eval_datasets([context_data + test_data])
+    return pacoh_task_eval
+
+
 class EvalNetwork:
 
     def __init__(
@@ -61,9 +76,10 @@ class EvalNetwork:
         self.meta_data = meta_data
         self.n_tasks = len(meta_data)
         self.n_samples_context = n_samples_context
-        self.pacoh_models = self.get_meta_trained_pacoh_models()
+        # self.pacoh_models = self.get_meta_trained_pacoh_models()
+        self.pacoh_models = []
 
-    def setup_data(self, task_num):
+    def setup_test_data(self, task_num):
         task_data = self.meta_data[task_num]
         context_ids = np.random.choice(
             len(task_data[0]), size=self.n_samples_context, replace=False
@@ -77,16 +93,16 @@ class EvalNetwork:
     def get_meta_trained_pacoh_models(self):
         pacoh_models = []
         for task in range(self.n_tasks):
-            pacoh_models.append(self.init_and_train_pacoh_models(task))
+            pacoh_models.append(self.setup_pacoh_model(task))
         return pacoh_models
 
-    def init_and_train_pacoh_models(self, task_id):
+    def setup_pacoh_model(self, task_id):
         meta_train_data = [self.meta_data[i] for i in range(self.n_tasks) if i != task_id]
         pacoh_model = PACOH_NN_Regression(
             meta_train_data,
             hidden_layer_sizes=(200, 200, 200, 200),
             num_iter_meta_train=60000,
-            num_iter_meta_test=5 * self.n_samples_context,
+            num_iter_meta_test=10 * self.n_samples_context,
             batch_size=min(8, self.n_samples_context),
             lr=1e-3,
             bandwidth=1.0
@@ -108,9 +124,9 @@ class EvalNetwork:
     def eval_bnn(self):
         bnn_eval_mean_per_task = []
         for task_num in range(self.n_tasks):
-            context_data, test_data = self.setup_data(task_num)
+            context_data, test_data = self.setup_test_data(task_num)
             bnn = self.setup_bnn(context_data)
-            bnn.fit(None, None, num_iter_fit=5*self.n_samples_context)
+            bnn.fit(None, None, num_iter_fit=10*self.n_samples_context)
             bnn_task_eval = bnn.eval(test_data[0], test_data[1])
             bnn_eval_mean_per_task.append(bnn_task_eval)
         metrics_mean, metrics_std = PACOH_NN_Regression._aggregate_eval_metrics_across_tasks(bnn_eval_mean_per_task)
@@ -119,7 +135,7 @@ class EvalNetwork:
     def eval_pacoh(self):
         pacoh_eval_mean_per_task = []
         for task_num in range(self.n_tasks):
-            context_data, test_data = self.setup_data(task_num)
+            context_data, test_data = self.setup_test_data(task_num)
             pacoh_model = self.pacoh_models[task_num]
             pacoh_task_eval, _ = pacoh_model.meta_eval_datasets([context_data + test_data])
             pacoh_eval_mean_per_task.append(pacoh_task_eval)
@@ -131,15 +147,18 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--n-samples-context", type=int, default=10)
+    parser.add_argument("--num-cpus", type=int, default=4)
 
     params = parser.parse_args()
+
+    ray.init(num_cpus=params.num_cpus)
 
     meta_data = load_meta_data()
 
     evaluator = EvalNetwork(meta_data, n_samples_context=params.n_samples_context)
 
     bnn_metrics_mean, bnn_metrics_std = evaluator.eval_bnn()
-    pacoh_metrics_mean, pacoh_metrics_std = evaluator.eval_pacoh()
+    pacoh_metrics_mean, pacoh_metrics_std = eval_pacoh(evaluator)
 
     print(f"\nBNN model metrics for {params.n_samples_context} context samples:\n")
     for key in bnn_metrics_mean:
@@ -148,3 +167,5 @@ if __name__ == "__main__":
     print(f"\nPACOH model metrics for {params.n_samples_context} context samples:\n")
     for key in pacoh_metrics_mean:
         print("%s: %.4f +- %.4f" % (key, pacoh_metrics_mean[key], pacoh_metrics_std[key]))
+
+    ray.shutdown()
