97007fb5ce35e2e11dcaf6a9378e10e23555ae7c
Arjun Bhardwaj <arbhardwaj98@gmail.com>
2023-05-25 15:31:28 +0200

    Added CEM PACOH to parser

--- experiments/meta_rl_experiments/parser.py
+++ experiments/meta_rl_experiments/parser.py
@@ -34,7 +34,7 @@ def get_argument_parser() -> argparse.ArgumentParser:
         "--agent-name",
         type=str,
         default="parallel_pacoh",
-        choices=["mpc", "rl2", "grbal", "parallel_grbal", "pacoh", "parallel_pacoh"]
+        choices=["mpc", "rl2", "grbal", "parallel_grbal", "pacoh", "parallel_pacoh", "cem_pacoh", "parallel_cem_pacoh"]
     )
     parser.add_argument(
         "--exploration",
